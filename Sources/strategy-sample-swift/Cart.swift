a959bba21c0027f27505b06d5a0502fcd548aeee
Tsungyu Yu <ytyubox@gmail.com>
2021-01-31 21:06:24 +0800

    refactoring: use switch cases instead of if/else if statement

--- Sources/strategy-sample-swift/Cart.swift
+++ Sources/strategy-sample-swift/Cart.swift
@@ -11,25 +11,26 @@ public class Cart {
     public init() {}
 
     public func shippingFee(_ shipper: String, _ length: Double, _ width: Double, _ height: Double, _ weight: Double) throws -> Double {
-        if shipper == "black cat" {
+        switch shipper {
+        case "black cat":
             if weight > 20 {
                 return 500
             } else {
                 return 100 + weight * 10
             }
-        } else if shipper == "hsinchu" {
+        case "hsinchu":
             let size: Double = length * width * height
             if length > 100 || width > 100 || height > 100 {
                 return size * 0.0000353 * 1100 + 500
             } else {
                 return size * 0.0000353 * 1200
             }
-        } else if shipper == "post office" {
+        case "post office":
             let feeByWeight: Double = 80 + weight * 10
             let size = length * width * height
             let feeBySize = size * 0.0000353 * 1100
             return feeByWeight < feeBySize ? feeByWeight : feeBySize
-        } else {
+        default:
             throw CartError("shipper not exist")
         }
     }
