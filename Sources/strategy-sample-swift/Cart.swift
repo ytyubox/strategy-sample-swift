6445dd70b0e5427c82f073d6374afef9eb0ceb0e
Tsungyu Yu <ytyubox@gmail.com>
2021-01-31 22:09:34 +0800

    refactoring: use HashMap instead of switch cases

--- Sources/strategy-sample-swift/Cart.swift
+++ Sources/strategy-sample-swift/Cart.swift
@@ -8,24 +8,14 @@ public class Cart {
         var errorDescription: String
     }
 
+    private let shippers: [String: Shipper] = [
+        "black cat": BlackCat(),
+        "hsinchu": Hsinchu(),
+        "post office": PostOffice(),
+    ]
     public init() {}
 
     public func shippingFee(_ shipperName: String, _ product: Product) throws -> Double {
-        return try getShipper(shipperName).calculateFee(product)
-    }
-
-    private func getShipper(_ shipperName: String) throws -> Shipper {
-        let shipper: Shipper
-        switch shipperName {
-        case "black cat":
-            shipper = BlackCat()
-        case "hsinchu":
-            shipper = Hsinchu()
-        case "post office":
-            shipper = PostOffice()
-        default:
-            throw CartError("shipper not exist")
-        }
-        return shipper
+        return shippers[shipperName]!.calculateFee(product)
     }
 }
