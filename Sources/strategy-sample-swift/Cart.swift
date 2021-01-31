8a1a2c13befaf8673d31d2551fbb08e712d3df24
Tsungyu Yu <ytyubox@gmail.com>
2021-01-31 21:51:26 +0800

    refactoring: extract delegate PostOffice class

--- Sources/strategy-sample-swift/Cart.swift
+++ Sources/strategy-sample-swift/Cart.swift
@@ -11,6 +11,7 @@ public class Cart {
     public init() {}
     private let blackCat = BlackCat()
     private let hsinchu = Hsinchu()
+    private let postOffice = PostOffice()
 
     public func shippingFee(_ shipper: String, _ product: Product) throws -> Double {
         switch shipper {
@@ -19,15 +20,9 @@ public class Cart {
         case "hsinchu":
             return hsinchu.calculateFee(product)
         case "post office":
-            return calculateFeeByPostOffice(product)
+            return postOffice.calculateFee(product)
         default:
             throw CartError("shipper not exist")
         }
-    }
-
-    private func calculateFeeByPostOffice(_ product: Product) -> Double {
-        let feeByWeight: Double = 80 + product.getWeight() * 10
-        let feeBySize = product.getSize() * 0.0000353 * 1100
-        return Swift.min(feeByWeight, feeBySize)
     }
 }
---
+++ Sources/strategy-sample-swift/PostOffice.swift
@@ -0,0 +1,17 @@
+//
+/*
+ *		Created by 游宗諭 in 2021/1/31
+ *
+ *		Using Swift 5.0
+ *
+ *		Running on macOS 10.15
+ */
+
+public class PostOffice {
+    public init() {}
+    internal func calculateFee(_ product: Product) -> Double {
+        let feeByWeight: Double = 80 + product.getWeight() * 10
+        let feeBySize = product.getSize() * 0.0000353 * 1100
+        return Swift.min(feeByWeight, feeBySize)
+    }
+}
